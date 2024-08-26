0cfd396982b65b6baf1492b35053a293831bc7bc
Neha-Gi <neha.girme@dci-student.org>
2024-08-26 21:04:20 +0200

    added views for cart

--- apps/purchase/views.py
+++ apps/purchase/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+#from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from apps.purchase.models import Cart
+from apps.purchase.serializer import CartSerializer
+
+#Function-base view
+@api_view(['GET']) # by default , it uses a 'GET' method
+def view_cart(request):
+    # Get all items using ORM
+    cart = Cart.objects.all()
+
+    # Deserialize using the CartSerializer
+    data = CartSerializer(cart,many=True)
+
+    return Response(data.data, status=status.HTTP_200_OK)
