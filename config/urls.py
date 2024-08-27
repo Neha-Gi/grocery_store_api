a58535d880ded1170f6b82f98e17a08de7d7dff8
Jendrik Soltau <jendrik.soltau90@gmail.com>
2024-08-27 16:16:40 +0200

    missing urls

--- config/urls.py
+++ config/urls.py
@@ -20,6 +20,6 @@ from django.urls import path , include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('api/v1/cart/',include('apps.purchase.urls',namespace = 'cart-urls'))
-
+    path('api/v1/cart/',include('apps.purchase.urls',namespace = 'cart-urls')),
+    path("api/v1/product/", include("apps.product.urls", namespace='product-url')),
 ]
